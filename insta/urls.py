a8f211258e5eff98dc873da2edebe859f3f287f7
Binamin-hussein100 <binamin.hassan@student.moringaschool.com>
2021-09-16 10:11:32 +0300

    added a logout url

--- insta/urls.py
+++ insta/urls.py
@@ -2,6 +2,10 @@ from django.urls import path,re_path
 from . import views
 
 urlpatterns = [
-    path('',views.index,name='Home'),
-    path('profile',views.get_profile,name = 'profile')
+    path('home/',views.index,name='home'),
+    path('search/',views.search, name='search'),
+    path('profile/',views.get_profile,name = 'profile'),
+    path('register/',views.register, name='register'),
+    path('',views.login, name='login'),
+    path('logout',views.logout_view, name='logout')
 ]
