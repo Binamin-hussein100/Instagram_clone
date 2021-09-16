02b275bca9a25187ea7921456022074f401d357f
Binamin-hussein100 <binamin.hassan@student.moringaschool.com>
2021-09-16 13:03:54 +0300

    os.environ

--- instagram/settings.py
+++ instagram/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-euhi+0fw-wnv7&vlxuzm-d!w$0%2k*jiuhdf2k%_a_s!ra1=r5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG =os.environ.get('DEBUG')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS')
 
-ALLOWED_HOSTS = []
 
 
 # Application definition
